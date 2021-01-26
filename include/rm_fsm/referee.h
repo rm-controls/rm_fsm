2553f2cdb507a0beb3736a3ca790bc68a938c5d7
qiayuan <liaoqiayuan@gmail.com>
2021-01-27 06:19:29 +0800

    change lp param

--- include/rm_fsm/referee.h
+++ include/rm_fsm/referee.h
@@ -6,8 +6,8 @@
 //Referee System
 #include <cstdint>
 #include <serial/serial.h>
-#include "rm_msgs/Referee.h"
-#include "rm_fsm/protocol.h"
+#include <rm_msgs/Referee.h>
+#include <rm_fsm/protocol.h>
 
 namespace referee {
 struct RefereeData {
