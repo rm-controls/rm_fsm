2175676ce2f8723376e67e16c685e76502d51e4a
Yanzhengying-18 <1697203829@qq.com>
2021-03-20 11:28:22 +0800

    Track mode in fsm

--- src/state_follow.cpp
+++ src/state_follow.cpp
@@ -36,17 +36,17 @@ void StateFollow<T>::run() {
     rate_pitch = this->data_->dbus_data_.m_y;
 
     if (this->data_->dbus_data_.p_r) {
-      this->setGimbal(rm_msgs::GimbalCmd::TRACK, 0.0, 0.0, 8);
+      this->setGimbal(rm_msgs::GimbalCmd::TRACK, 0.0, 0.0, 5);
     } else {
       this->setGimbal(rm_msgs::GimbalCmd::RATE, rate_yaw, rate_pitch, 0);
     }
 
     if (this->data_->dbus_data_.p_l) {
       this->data_->shooter_heat_limit_->input(this->data_->referee_->referee_data_, this->shoot_hz_);
-      this->setShoot(rm_msgs::ShootCmd::PUSH, rm_msgs::ShootCmd::SPEED_16M_PER_SECOND,
-                     this->data_->shooter_heat_limit_->output(), now);
+      this->setShoot(rm_msgs::ShootCmd::PUSH, rm_msgs::ShootCmd::SPEED_15M_PER_SECOND,
+                     5, now);
     } else {
-      this->setShoot(rm_msgs::ShootCmd::PASSIVE, rm_msgs::ShootCmd::SPEED_10M_PER_SECOND, 0.0, now);
+      this->setShoot(rm_msgs::ShootCmd::PASSIVE, rm_msgs::ShootCmd::SPEED_15M_PER_SECOND, 0.0, now);
     }
 
   } else { // rc control
@@ -63,20 +63,20 @@ void StateFollow<T>::run() {
     rate_yaw = -this->data_->dbus_data_.ch_l_x;
     rate_pitch = -this->data_->dbus_data_.ch_l_y;
 
-    if (this->data_->dbus_data_.s_l == rm_msgs::DbusData::UP) {
-      this->setGimbal(rm_msgs::GimbalCmd::TRACK, 0.0, 0.0, 8);
+    if (this->data_->dbus_data_.s_r == rm_msgs::DbusData::UP) {
+      this->setGimbal(rm_msgs::GimbalCmd::TRACK, 0.0, 0.0, 5);
     } else {
       this->setGimbal(rm_msgs::GimbalCmd::RATE, rate_yaw, rate_pitch, 0);
     }
 
     if (this->data_->dbus_data_.s_l == rm_msgs::DbusData::UP) {
       this->data_->shooter_heat_limit_->input(this->data_->referee_->referee_data_, this->shoot_hz_);
-      this->setShoot(rm_msgs::ShootCmd::PUSH, rm_msgs::ShootCmd::SPEED_10M_PER_SECOND,
-                     this->data_->shooter_heat_limit_->output(), now);
+      this->setShoot(rm_msgs::ShootCmd::PUSH, rm_msgs::ShootCmd::SPEED_15M_PER_SECOND,
+                     5, now);
     } else if (this->data_->dbus_data_.s_l == rm_msgs::DbusData::MID) {
-      this->setShoot(rm_msgs::ShootCmd::READY, rm_msgs::ShootCmd::SPEED_10M_PER_SECOND, 0.0, now);
+      this->setShoot(rm_msgs::ShootCmd::READY, rm_msgs::ShootCmd::SPEED_15M_PER_SECOND, 0.0, now);
     } else {
-      this->setShoot(rm_msgs::ShootCmd::PASSIVE, rm_msgs::ShootCmd::SPEED_10M_PER_SECOND, 0.0, now);
+      this->setShoot(rm_msgs::ShootCmd::PASSIVE, rm_msgs::ShootCmd::SPEED_15M_PER_SECOND, 0.0, now);
     }
   }
 }
