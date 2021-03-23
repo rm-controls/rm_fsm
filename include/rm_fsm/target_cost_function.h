4d35bef5aa31d1fbc3809beb40f740aa8684c344
ChenWEIx2 <1374691864@qq.com>
2021-03-23 20:58:24 +0800

    Get gimbal_transformStamped to calculate cost

--- include/rm_fsm/target_cost_function.h
+++ include/rm_fsm/target_cost_function.h
@@ -7,20 +7,27 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
-#include "rm_msgs/TrackData.h"
+#include <tf2_ros/transform_listener.h>
+#include "rm_msgs/TargetDetection.h"
+#include "rm_msgs/TargetDetectionArray.h"
 
 class TargetCostFunction {
  public:
-  TargetCostFunction(ros::NodeHandle &nh);
-  void input(rm_msgs::TrackData track_data);
+  explicit TargetCostFunction(ros::NodeHandle &nh);
+  void input(rm_msgs::TargetDetectionArray target_detection_array);
   int output() const;
-  double getCost(geometry_msgs::Twist twist, geometry_msgs::Pose pose);
+  double calculateCost(rm_msgs::TargetDetection target_detection);
 
  private:
   int id_;
   double cost_ = 1000000;
   double time_interval_;
   geometry_msgs::Pose pose_self_;
+  ros::Subscriber self_pose_sub_;
+
+  tf2_ros::Buffer tf_;
+  tf2_ros::TransformListener *tf_listener_;
+  geometry_msgs::TransformStamped gimbal_transformStamped;
 
 };
 
--- src/target_cost_function.cpp
+++ src/target_cost_function.cpp
@@ -6,33 +6,38 @@
 
 TargetCostFunction::TargetCostFunction(ros::NodeHandle &nh) {
   ros::NodeHandle cost_nh = ros::NodeHandle(nh, "target_cost_function");
-
   cost_nh.param("time_interval_", time_interval_, 0.1);
 
+  this->tf_listener_ = new tf2_ros::TransformListener(this->tf_);
 }
 
-void TargetCostFunction::input(rm_msgs::TrackData track_data) {
-  int total_number = track_data.name.size();
-  double cost_temp = 0;
-  for (int i = 0; i < total_number; i++) {
-    cost_temp = getCost(track_data.speed[i], track_data.pose[i]);
+void TargetCostFunction::input(rm_msgs::TargetDetectionArray target_detection_array) {
+
+  try {
+    gimbal_transformStamped = this->tf_.lookupTransform("odom", "link_pitch", ros::Time(0));
+  }
+  catch (tf2::TransformException &ex) {
+    //ROS_ERROR("%s",ex.what());
+  }
+
+  int target_numbers = target_detection_array.detections.size();
+  double cost_temp;
+  for (int i = 0; i < target_numbers; i++) {
+    cost_temp = calculateCost(target_detection_array.detections[i]);
     if (cost_temp <= cost_) {
       cost_ = cost_temp;
-      id_ = i;
+      id_ = target_detection_array.detections[i].id;
     }
   }
 }
 
-double TargetCostFunction::getCost(geometry_msgs::Twist twist, geometry_msgs::Pose pose) {
-  //0.1s later ,the distance of target and robot
-  double target_later_pose_x = pose.position.x + time_interval_ * twist.linear.x;
-  double target_later_pose_y = pose.position.y + time_interval_ * twist.linear.y;
-  double target_later_pose_z = pose.position.z + time_interval_ * twist.linear.z;
-  double delta_x_2 = pow(pose_self_.position.x - target_later_pose_x, 2);
-  double delta_y_2 = pow(pose_self_.position.y - target_later_pose_y, 2);
-  double delta_z_2 = pow(pose_self_.position.z - target_later_pose_z, 2);
+double TargetCostFunction::calculateCost(rm_msgs::TargetDetection target_detection) {
+  double delta_x_2 = pow((gimbal_transformStamped.transform.translation.x - target_detection.pose.position.x), 2);
+  double delta_y_2 = pow((gimbal_transformStamped.transform.translation.y - target_detection.pose.position.y), 2);
+  double delta_z_2 = pow((gimbal_transformStamped.transform.translation.z - target_detection.pose.position.z), 2);
 
-  double cost = sqrt(delta_x_2 + delta_y_2 + delta_z_2);
+  double distance = sqrt(delta_x_2 + delta_y_2 + delta_z_2);
+  double cost = distance * target_detection.confidence;
 
   return cost;
 }
