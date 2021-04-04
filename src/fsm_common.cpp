00fad552caf68560a6389c256932c38a2d4fb0d8
astro <1015926336@qq.com>
2021-04-05 01:27:28 +0800

    Update to show power manager data on client

--- src/fsm_common.cpp
+++ src/fsm_common.cpp
@@ -150,10 +150,20 @@ Fsm<T>::Fsm(ros::NodeHandle &node_handle):nh_(node_handle) {
 template<typename T>
 void Fsm<T>::run() {
   // TODO: Safety check
+  char power_string[30] = {' '};
+  float power_float;
 
   // run referee system and publish some referee data
   if (data_.referee_->flag_) {
     data_.referee_->read();
+    power_float = data_.referee_->power_manager_data_.parameters[3]*100;
+    sprintf(power_string, "%.2f", power_float);
+    if (power_float >= 0.6)
+      data_.referee_->drawCharacter(data_.referee_->robot_id_, data_.referee_->client_id_, 1, kGreen, kAdd, power_string);
+    else if (power_float < 0.6 && power_float >= 0.3)
+      data_.referee_->drawCharacter(data_.referee_->robot_id_, data_.referee_->client_id_, 1, kYellow, kAdd, power_string);
+    else if (power_float < 0.3)
+      data_.referee_->drawCharacter(data_.referee_->robot_id_, data_.referee_->client_id_, 1, kOrange, kAdd, power_string);
   }
 
   // Run the robot control code if operating mode is not unsafe
