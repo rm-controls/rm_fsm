8aba3400c632cf2013faa1a4136617fb51cea721
chenzheng <1047979458@qq.com>
2021-04-10 03:36:28 +0800

    only attack id 1 and 3

--- src/state_automatic.cpp
+++ src/state_automatic.cpp
@@ -87,19 +87,14 @@ void StateAutomatic<T>::run() {
     this->data_->target_cost_function_->input(this->data_->track_data_array_);
     attack_id_ = this->data_->target_cost_function_->output();
     //shooter control
-    if (attack_id_ != 0 && std::abs(this->data_->gimbal_des_error_.error_yaw) < this->gimbal_error_limit_
+    if ((attack_id_ == 1 || attack_id_ == 3)
+        && std::abs(this->data_->gimbal_des_error_.error_yaw) < this->gimbal_error_limit_
         && std::abs(this->data_->gimbal_des_error_.error_pitch) < this->gimbal_error_limit_) {
       this->setShoot(rm_msgs::ShootCmd::PUSH,
                      30,
                      this->data_->shooter_heat_limit_->output(),
                      now);
-    }/* else if (now - last_time_ > ros::Duration(0.5)) {
-      this->setShoot(rm_msgs::ShootCmd::PUSH,
-                     30,
-                     this->data_->shooter_heat_limit_->output(),
-                     now);
-    } */
-    else {
+    } else {
       if (now - last_time_ > ros::Duration(0.5))
         this->setShoot(rm_msgs::ShootCmd::READY, 30, 0, now);
     }
@@ -137,7 +132,7 @@ void StateAutomatic<T>::run() {
 
 
     //gimbal control
-    if (attack_id_ != 0) {
+    if (attack_id_ == 1 || attack_id_ == 3) {
       this->setGimbal(rm_msgs::GimbalCmd::TRACK, 0, 0, attack_id_, 30);
       last_time_ = now;
     } else {
