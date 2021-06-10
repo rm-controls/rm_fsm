9f523a98835966802c04ddc38683573ff85404d1
Peter-Chan-tech <302646496@qq.com>
2021-06-10 15:22:44 +0800

    Add track timeout

--- include/rm_fsm/state_raw.h
+++ include/rm_fsm/state_raw.h
@@ -17,14 +17,19 @@ class StateRaw : public StateBase {
     vel_2d_cmd_sender_->setLinearXVel(data_->dbus_data_.ch_r_y);
   }
   void setGimbal() override {
-    if (data_->dbus_data_.s_l == rm_msgs::DbusData::DOWN) {
-      gimbal_cmd_sender_->setMode(rm_msgs::GimbalCmd::RATE);
-      gimbal_cmd_sender_->setRate(-data_->dbus_data_.ch_l_x, -data_->dbus_data_.ch_l_y);
-    } else gimbal_cmd_sender_->setMode(rm_msgs::GimbalCmd::TRACK);
+    gimbal_cmd_sender_->setRate(-data_->dbus_data_.ch_l_x, -data_->dbus_data_.ch_l_y);
+    if (data_->dbus_data_.s_l == rm_msgs::DbusData::DOWN) { gimbal_cmd_sender_->setMode(rm_msgs::GimbalCmd::RATE); }
+    else {
+      gimbal_cmd_sender_->setMode(rm_msgs::GimbalCmd::TRACK);
+      gimbal_cmd_sender_->setBulletSpeed(shooter_cmd_sender_->getSpeed());
+      gimbal_cmd_sender_->updateCost(data_->track_data_array_, false);
+    }
   }
   void setShooter() override {
-    if (data_->dbus_data_.s_l == rm_msgs::DbusData::UP) shooter_cmd_sender_->setMode(rm_msgs::ShootCmd::PUSH);
-    else if (data_->dbus_data_.s_l == rm_msgs::DbusData::MID) shooter_cmd_sender_->setMode(rm_msgs::ShootCmd::READY);
+    if (data_->dbus_data_.s_l == rm_msgs::DbusData::UP) {
+      shooter_cmd_sender_->setMode(rm_msgs::ShootCmd::PUSH);
+      shooter_cmd_sender_->checkError(data_->gimbal_des_error_.error);
+    } else if (data_->dbus_data_.s_l == rm_msgs::DbusData::MID) shooter_cmd_sender_->setMode(rm_msgs::ShootCmd::READY);
     else shooter_cmd_sender_->setMode(rm_msgs::ShootCmd::STOP);
   }
 };
